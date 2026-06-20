a9e4f7b5e41ef43c8806bcb3517c8c2c200361a5
KartikeyaPathak6 <pathakkartikeya55@gmail.com>
2026-06-20 21:51:36 +0530

    A bank it is

---
+++ CS50P Problem Sets/Week_1/Home Federal Savings Bank/bank.py
@@ -0,0 +1,11 @@
+def main():
+    x=input("Greetings: ").strip().lower()
+    return x
+
+def con():
+    if x == "hello":
+    print("$0")
+    elif x.startswith("h"):
+        print("$20")
+    else:
+        print("$100")
